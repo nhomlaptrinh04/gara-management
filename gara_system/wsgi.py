709d5d1ebd94b69f438adfc76e0a811838bf52ea
Ten Cua Ban <email_cua_ban@gmail.com>
2026-09-04 15:42:56 +0700

    Cap nhat wsgi tu dong tao user

--- gara_system/wsgi.py
+++ gara_system/wsgi.py
@@ -14,3 +14,30 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gara_system.settings')
 
 application = get_wsgi_application()
+import os
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gara_system.settings')
+application = get_wsgi_application()
+
+# Tự động tạo tài khoản nhân viên khi app khởi động trên Render
+try:
+    from django.contrib.auth.models import User, Group
+    staff_accounts = [
+        ("Thukho01", "ThuKho"),
+        ("Letan05", "LeTan"),
+        ("Thungan02", "KeToan"),
+        ("Thosuachua03", "ThoMay"),
+        ("Quandoc04", "QuanLyKyThuat"),
+        ("chamsockh06", "CSKH"),
+    ]
+    for username, group_name in staff_accounts:
+        user, created = User.objects.get_or_create(username=username)
+        if created:
+            user.set_password("MatKhauMacDinh123@")
+            user.is_staff = True
+            user.save()
+        group, _ = Group.objects.get_or_create(name=group_name)
+        user.groups.add(group)
+except Exception:
+    pass
